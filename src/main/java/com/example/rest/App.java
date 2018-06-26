3ab297b02bdcc0def53bf66787eb28aabc92acd0
smorale5 <s.morales.velasquez@accenture.com>
2018-06-26 11:22:23 +0200

    Update App.java

--- src/main/java/com/example/rest/App.java
+++ src/main/java/com/example/rest/App.java
@@ -51,7 +51,7 @@ public class App {
     private static void runGetRequest() {
  
     	// sample URL
-        String url = "140.86.15.104:3000/fighters/33/45/red/smorale5";
+        String url = "140.86.15.104:3000/fighters/45/0/red/smorale5";
         CloseableHttpResponse response = null;
  
         try {
