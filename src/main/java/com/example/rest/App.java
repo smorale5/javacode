b0712958e0f1b404709088db0973049eb5451b90
smorale5 <s.morales.velasquez@accenture.com>
2018-06-26 11:54:47 +0200

    Update App.java

--- src/main/java/com/example/rest/App.java
+++ src/main/java/com/example/rest/App.java
@@ -1,33 +1,32 @@
-// My microservice!
 /* Copyright © 2016 Oracle and/or its affiliates. All rights reserved. */
+// My microservice!
 package com.example.rest;
- 
+
 import java.util.Optional;
 import java.util.Properties;
 import java.io.IOException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
- 
+
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.util.EntityUtils;
- 
-@SpringBootApplication
 
+@SpringBootApplication
 public class App {
- 
+
     // Get PORT and HOST from Environment or set default
     public static final Optional<String> host;
     public static final Optional<String> port;
     public static final Properties myProps = new Properties();
- 
+
     static {
         host = Optional.ofNullable(System.getenv("HOSTNAME"));
         port = Optional.ofNullable(System.getenv("PORT"));
     }
- 
+
     /**
      * Start the server.
      */
@@ -35,48 +34,56 @@ public class App {
         // Set properties
         myProps.setProperty("server.address", host.orElse("localhost"));
         myProps.setProperty("server.port", port.orElse("8080"));
- 
+
         SpringApplication app = new SpringApplication(App.class);
         app.setDefaultProperties(myProps);
         app.run(args);
- 
+
         System.out.println("Server started");
- 
+
         runGetRequest();
         runDatabaseQuery();
     }
- 
+
     /**
      * Performs a simple GET request and prints the result to the log.
      */
- private static void runGetRequest() {
-  for (int i=9;i>=0;i--){
-   //sample URL
-   //String url = "http://ip.jsontest.com/";
-   //String url = "http://140.86.15.104:3000/shield/33/45/red/smorale5/";
-   String YY=String.valueOf(i); 
-   String url="http://140.86.15.104:3000/fighters/45/"+YY+"/red/smorale5/";
-   CloseableHttpResponse response = null;
-   try {
-                  CloseableHttpClient httpclient = HttpClients.createDefault();
-                  HttpGet httpGet = new HttpGet(url);
-                  response = httpclient.execute(httpGet);
-                  String content = EntityUtils.toString(response.getEntity());
-                  System.out.println("Server response: " + content);
-              } catch (IOException e) {
-                  System.out.println(e);
-              } finally {
-                  try {
-                      if (response != null) {
-                          response.close();
-                      }
-                  } catch (IOException ie) {
-                      System.out.println(ie);
-                  }
-              } 
-  }
- }
- 
+    private static void runGetRequest() {
+
+  
+               for (int i=9; i>=0; i--)
+     {
+        String dummyStr = String.valueOf(i); 
+        String url = "http://140.86.15.104:3000/fighters/45/" + dummyStr + "/red/smorale5/";
+
+        CloseableHttpResponse response = null;
+
+        try {
+            CloseableHttpClient httpclient = HttpClients.createDefault();
+            HttpGet httpGet = new HttpGet(url);
+            response = httpclient.execute(httpGet);
+            String content = EntityUtils.toString(response.getEntity());
+            System.out.println("Server response: " + content);
+        } catch (IOException e) 
+        {
+            System.out.println(e);
+        } finally 
+        
+        {
+            try 
+            {
+                if (response != null) 
+                   {
+                    response.close();
+                   }
+            } catch (IOException ie) 
+                   {
+                     System.out.println(ie);
+                   }
+        }    
+    }
+    }
+
     /**
      * Performs a call to the database.
      */
